a1877fcf5cdbb62f12a485cf0ef28838cc7f4dcc
Giulianno Ramos <giuleramos@gmail.com>
2023-05-25 01:25:07 -0300

    Adding comments

--- src/Univali.Api/Data.cs
+++ src/Univali.Api/Data.cs
@@ -1,40 +1,51 @@
 using Univali.Api.Entities;
+using System.Collections.Generic;
 
 namespace Univali.Api
 {
-    //classe sem parametro, nao possui tipos como parametro
     public class Data
     {
-        public List<Customer> Customers {get; set;}
-        //propriedade privada que contem a unica referencia a instancia              
+        // Propriedade que armazena a lista de clientes
+        public List<Customer> Customers { get; set; }
+
+        // Variável estática que armazena a única instância da classe Data
         private static Data? _instance;
 
-        
-
+        // Propriedade estática Instance que representa a única instância da classe Data
+        // acessível globalmente usando o padrão Singleton.
         public static Data Instance
         {
             get
-            {               
-                return _instance?? = new Data();
+            {
+                if (_instance == null)
+                {
+                    // Se _instance for nulo, cria uma nova instância de Data e atribui à variável _instance.
+                    _instance = new Data();
+                }
+                // Retorna a instância existente ou a nova instância criada.
+                return _instance;
             }
         }
 
+        // Construtor privado para evitar que a classe seja instanciada externamente.
         private Data()
         {
-            new List<Customer>
+            // Inicializa a lista de Customers com alguns exemplos de clientes.
+            Customers = new List<Customer>
             {
-                new Customer{
+                new Customer
+                {
                     Id = 1,
                     Name = "Linus",
                     Cpf = "123456789"
                 },
-                new Customer{
+                new Customer
+                {
                     Id = 2,
                     Name = "Bill",
                     Cpf = "987654321"
                 }
             };
         }
-
     }
 }
