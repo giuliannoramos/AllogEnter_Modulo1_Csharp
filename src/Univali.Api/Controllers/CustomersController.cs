da9340abb03fe0316c9520b90b45b009b8c8bc91
Giulianno <giuleramos@gmail.com>
2023-05-26 16:03:51 -0300

    Add get id

--- src/Univali.Api/Controllers/CustomersController.cs
+++ src/Univali.Api/Controllers/CustomersController.cs
@@ -9,8 +9,22 @@ public class CustomersController : ControllerBase
 {
     public ActionResult<IEnumerable<Customer>> GetCustomers()
     {
-        var result = Data.Instance.Customers;        
-        
+        var result = Data.Instance.Customers;
+
         return Ok(result);
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<Customer> GetCustomer([FromRoute] int id)
+    {
+        var customer = Data.Instance.Customers.FirstOrDefault(c => c.Id == id);
+
+        if(customer == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(customer);        
+    }
 }
+
